b64827bc64f8fddf526cf3b0418f9f3847d8ac01
Sathiya C <>
2020-07-24 12:12:31 +0800

    some changes

--- spring-boot-modules/spring-boot-hibernate-multiple-datasource/src/main/java/com/kathirmunai/cms/multiple/ds/MultipleDatasourceApplication.java
+++ spring-boot-modules/spring-boot-hibernate-multiple-datasource/src/main/java/com/kathirmunai/cms/multiple/ds/MultipleDatasourceApplication.java
@@ -1,11 +1,9 @@
 package com.kathirmunai.cms.multiple.ds;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class MultipleDatasourceApplication {
 
 	public static void main(String[] args) {	
